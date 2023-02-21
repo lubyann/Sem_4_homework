d6ddbe467ac32e4d0b3a4c437daa60d09d48a90c
Maksim Lubyannikov <m.lubyann@gmail.com>
2023-02-21 18:21:38 +0300

    add task24

---
+++ Task24/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+
+
+int GetSum(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+        sum += i; // sum = sum + i
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Enter a number: ");
+int n = int.Parse(Console.ReadLine()!);
+// GetSum(n);
+//int result = GetSum(n);
+Console.WriteLine($"Сумма от 1 до {n} равна {GetSum(n)}");
