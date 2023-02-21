9849d2d7e41b7e581d3610891273e4b7ba471a35
Maksim Lubyannikov <m.lubyann@gmail.com>
2023-02-21 18:22:32 +0300

    add task28

---
+++ Task28/Program.cs
@@ -0,0 +1,27 @@
+﻿//28 Напишите программу, которая принимает на вход число N 
+//и выдаёт произведение чисел от 1 до N.
+
+//4-> 24
+//5-> 120 == 1 * 2 * 3 * 4 * 5
+
+int Number(int n)
+{
+    int count = 1;
+    int i = 2;
+    if (n < 0)
+    {
+        i = n;
+        n = -1;
+    }
+    for (; i <= n; i++)
+    {
+        count = count * i;
+    }
+    return count;
+}
+
+Console.Clear();
+Console.Write("Enter a number: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{n} -> {Number(n)}");
+
