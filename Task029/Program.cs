8c0f3094b76efefd54b2893213b0fb579a7ea0d7
Maksim Lubyannikov <m.lubyann@gmail.com>
2023-02-22 21:27:51 +0300

    add task29

--- Task029/Program.cs
+++ Task029/Program.cs
@@ -29,3 +29,43 @@ int[] GetBinaryArray(int size)
 // foreach(var el in array){
 //     Console.Write($"{el} ");
 // }
+
+// РЕШЕНИЕ ЭТАЛОН
+
+// // Ввод числа с экрана
+// int Prompt(string message)
+// {
+//     Console.Write(message); // Выводим приглашение ко вводу
+//     string ReadInput = Console.ReadLine();
+//     int result = int.Parse(ReadInput); // приводим к числу
+//     return result; // Возвращаем результат
+// }
+
+// // Метод для получения случайных значений массива
+// int[] GenerateArray(int Length, int minValue, int maxValue)
+// {
+//     int[] array = new int[Length]; // объявляем массив
+//     Random random = new Random();
+//     for (int i = 0; i < Length; i++)
+//     {
+//         array[i] = random.Next(minValue, maxValue + 1); // заполняем случайными цифрами из диапазона SatrArr до EndArr
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] array);
+// {
+//     Console.Write("[");
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         Console.Write($"{array[i]}, "); // вывод значения массива
+//     }
+//     Console.Write($"{array[array.Lenght - 1]}"); // вывод значения массива
+//     Console.Write("]");
+// }
+
+// int lenght = Prompt("Длина массива: ");
+// int min = Prompt("Начальное значение, для диапазона случайного числа: ");
+// int max = Prompt("Конечное значение, для диапазона случайного числа: ");
+// int[] array = GenerateArray(lenght, min, max); // заполнение массива случайными числами
+// PrintArray(array); // Вывод массива
