db36af812ac3c89c94a64d580455c33c990b40f1
Maksim Lubyannikov <m.lubyann@gmail.com>
2023-03-09 16:59:20 +0300

    add task39

---
+++ sem5_task039/Program.cs
@@ -0,0 +1,48 @@
+﻿//Задача 39
+// Напишите программу которая перевернет одномерный массив
+//(последний элемент будет на первом месте, а первый на последнем и т.д.)
+
+
+Console.Clear();
+
+int[] array = GetArray(10, 0, 10);
+Console.WriteLine(String.Join(" ", array));
+
+int[] reversArray = ReversArray2(array);
+Console.WriteLine(String.Join(" ", reversArray));
+
+ReversArray1(array);
+Console.WriteLine(String.Join(" ", array));
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+void ReversArray1(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length / 2; i++)
+    {
+        int k = inArray[i];
+        inArray[i] = inArray[inArray.Length - i - 1];
+        inArray[inArray.Length - i - 1] = k;
+    }
+}
+
+int[] ReversArray2(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        result[i] = inArray[inArray.Length - 1 - i];
+
+    }
+    return result;
+}
