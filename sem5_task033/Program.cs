6ab03b7c7e485c4836f26c734554c485662b8303
Maksim Lubyannikov <m.lubyann@gmail.com>
2023-03-09 17:03:01 +0300

    add 33

--- sem5_task033/Program.cs
+++ sem5_task033/Program.cs
@@ -19,10 +19,10 @@
 
 Console.Clear();
 Console.Write("Введите элементы массива через пробел: ");
-string elements = Console.ReadLine();
+string elements = Console.ReadLine()!;
 int[] baseArray = GetArrayFromString(elements);
 Console.Write("Введите число: ");
-int n = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine()!);
 if (FindElement(baseArray, n))
 {
     Console.WriteLine("Да");
